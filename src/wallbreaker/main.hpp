076ed8e699c9112b1bd5f7572b3bf95eefe06604
Andy Mender <andymenderunix@gmail.com>
2022-08-23 22:57:30 +0200

    Add comments on X, Y, Z coords

--- src/wallbreaker/main.hpp
+++ src/wallbreaker/main.hpp
@@ -7,13 +7,14 @@
 namespace Main { 
     // constants
     // TODO: Drop 'constexpr' and make adjustable from Menu?
-    constexpr int SCREEN_WIDTH = 800;
-    constexpr int SCREEN_HEIGHT = 450;
+    constexpr int SCREEN_WIDTH = 800;       // X
+    constexpr int SCREEN_HEIGHT = 450;      // Y
 
-    constexpr float FIELD_WIDTH = 10.0f;
-    constexpr float FIELD_HEIGHT = 5.0f;
-    constexpr float FIELD_DEPTH = 15.0f;    
+    constexpr float FIELD_WIDTH = 10.0f;    // X
+    constexpr float FIELD_HEIGHT = 5.0f;    // Y
+    constexpr float FIELD_DEPTH = 15.0f;    // Z
 
+    // X, Y, Z
     constexpr Vector3 ORIGIN_POS = { 0.0f, 0.0f, 0.0f };     // Use as reference point to position other objects
 
  }   // namespace Main
